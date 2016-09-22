2b6f66c531cbe450265b0446bd798c6461edbf07
Guenter Bailey <office@bailey-solution.com>
2016-09-22 08:36:24 +0200

    alte codes und Tests entfernt

--- USB_to_TTL_Serial/USB_to_TTL_Serial.ino
+++ USB_to_TTL_Serial/USB_to_TTL_Serial.ino
@@ -1,78 +1,8 @@
 /*
-  Software serial multple serial test
-
-  Receives from the hardware serial, sends to software serial.
-  Receives from software serial, sends to hardware serial.
-
-  The circuit:
-   RX is digital pin 10 (connect to TX of other device)
-   TX is digital pin 11 (connect to RX of other device)
-
-  Note:
-  Not all pins on the Mega and Mega 2560 support change interrupts,
-  so only the following can be used for RX:
-  10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
-  Not all pins on the Leonardo and Micro support change interrupts,
-  so only the following can be used for RX:
-  8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
-  created back in the mists of time
-  modified 25 May 2012
-  by Tom Igoe
-  based on Mikal Hart's example
-
-  This example code is in the public domain.
-
-*/
-
-/*
-  #include <SoftwareSerial.h>
-
-  SoftwareSerial mySerial(10, 11); // RX, TX
-
-  void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  //Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(115200);
-  //mySerial.println("Hello, world?");
-  }
-
-  void loop() {
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
-  }
-*/
-/*
-  #include <SoftwareSerial.h>
-  SoftwareSerial Serial1(10, 11); // RX, TX
-  void setup()
-  {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW); // to turn the led off
-    Serial.begin(115200); // from Due to PC connection (maybe 57600)
-    Serial1.begin(115200); // from esp-01 to Due connection (maybe 57600)
-  }
-
-  void loop()
-  {
-    if (Serial1.available())
-        Serial.write(Serial1.read());
-    if (Serial.available())
-        Sereial1.write(Serial.read());
-    //delay(10); // feel free to adjust this or remove it to suit your need! (spin delay).
-  }
-*/
+ * Dieser Sketch schaltet die Pins 0,1 RX,TX auf INPUT um.
+ * Somit kann nicht aus Versehen eine Spannung zurückkommen.
+ * 
+ */
 void setup() {
   pinMode(0,INPUT);
   pinMode(1,INPUT);
